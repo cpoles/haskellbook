4810e5ec326f67065be7d4a40d02cc83140f8342
cpoles <carlos.poles@alumni.griffithuni.edu.au>
2019-11-20 22:24:57 +1000

    Finished Parametricity Section

---
+++ chapter05/parametricity.hs
@@ -0,0 +1,33 @@
+module Parametricity where
+
+
+-- Parametricity
+-- 1. Given the type a -> a, which is the type for id, attempt
+-- to make a function that terminates successfully that does
+-- something other than returning the same value. This is
+-- impossible, but you should try it anyway.
+
+f :: a -> a
+f = \x -> x
+
+-- 2. We can get a more comfortable appreciation of parametricity
+-- by looking at a -> a -> a. This hypothetical
+-- function a -> a -> a has two–and only two–implementations.
+-- Write both possible versions of a -> a -> a. After
+-- doing so, try to violate the constraints of parametrically
+-- polymorphic values we outlined above.
+
+g :: a -> a -> a
+g x = \y -> x
+
+-- or 
+-- g x y = y
+
+-- 3. Implement a -> b -> b. How many implementations can
+-- it have? Does the behavior change when the types of 𝑎
+-- and 𝑏 change?
+
+h :: a -> b -> b
+h x = \y -> y
+-- or
+-- h x y = y
