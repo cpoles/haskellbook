2e1a82cf32aae3a26107ba48be1729e14b80e766
cpoles <carlos.poles@alumni.griffithuni.edu.au>
2019-11-27 20:42:55 +1000

    removed tab chars

--- chapter07/casePractice.hs
+++ chapter07/casePractice.hs
@@ -3,8 +3,8 @@ module CasePractice where
 
 functionC :: (Ord a) => a -> a -> a
 functionC x y = case x > y of
-									True -> x
-									False -> y
+                  True -> x
+                  False -> y
 
 ifEvenAdd2 :: (Integral a) => a -> a
 ifEvenAdd2 n = case even n of
